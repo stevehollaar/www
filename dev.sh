8952bf7cd1cbb62650ebeb3fba6a9115d51512f0
Steve Hollaar <steve.hollaar@gmail.com>
2014-03-26 09:42:18 -0700

    Super-secret script for key variables

--- dev.sh
+++ dev.sh
@@ -1,1 +1,8 @@
+echo 'Running npm install...'
+npm install
+
+echo 'Setting key environment variables...'
+sh ./set_keys.sh
+
+echo 'Starting local dev server...'
 NODE_ENV=development MONGO_URL=mongodb://localhost/www node --harmony server/app.js
