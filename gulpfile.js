0af63dec90b380479bb24448828e493f9c608a19
Steve Hollaar <steve.hollaar@gmail.com>
2014-05-17 17:45:48 -0700

    Get JSHint working properly

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var resolveDependencies = require('gulp-resolve-dependencies');
@@ -9,16 +10,20 @@ var defineModule = require('gulp-define-module');
 var declare = require('gulp-declare');
 var rename = require('gulp-rename');
 
+var JS_DIRS = ['client/js/*.js', 'client/js/*/*.js'];
+var SASS_DIRS = ['client/scss/*.scss']
+var HBS_DIRS = ['client/hbs/*.hbs'];
+
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src('js/*.js')
+    return gulp.src(JS_DIRS)
         .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+        .pipe(jshint.reporter(stylish));
 });
 
 // Compile Sass
 gulp.task('sass', function() {
-    return gulp.src('client/scss/*.scss')
+    return gulp.src(SASS_DIRS)
         .pipe(sass({errLogToConsole: true}))
         .pipe(concat('main.css'))
         .pipe(gulp.dest('static/css'));
@@ -26,7 +31,7 @@ gulp.task('sass', function() {
 
 // Process client-side templates
 gulp.task('templates', function(){
-    return gulp.src(['client/hbs/*.hbs'])
+    return gulp.src(HBS_DIRS)
         .pipe(handlebars())
         .pipe(defineModule('plain'))
         .pipe(declare({
@@ -38,7 +43,7 @@ gulp.task('templates', function(){
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src(['client/js/*.js', 'client/js/*/*.js'])
+    return gulp.src(JS_DIRS)
         .pipe(resolveDependencies({
             pattern: /\* @requires [\s-]*(.*?\.js)/g
         }))
@@ -51,9 +56,9 @@ gulp.task('scripts', function() {
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('client/hbs/*.hbs', ['templates']);
-    gulp.watch(['client/js/*.js', 'client/js/*/*.js'], ['lint', 'scripts']);
-    gulp.watch('client/scss/*.scss', ['sass']);
+    gulp.watch(HBS_DIRS, ['templates']);
+    gulp.watch(JS_DIRS, ['lint', 'scripts']);
+    gulp.watch(SASS_DIRS, ['sass']);
 });
 
 // Default Task
